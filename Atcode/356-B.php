a81c7e69288bb126729fc2ba22158eeb044763d6
akiyama <k.akiyama@willdo.co.jp>
2025-01-15 14:32:37 +0900

    回答記入

--- Atcode/356-B.php
+++ Atcode/356-B.php
@@ -1,2 +1,26 @@
 <?php
 
+fscanf(STDIN, "%d %d %d", $N, $M);
+// 目標値
+$goal = array_map('intval', explode(' ', trim(fgets(STDIN))));
+
+$total =[];
+
+for ($i = 0; $i < $M; $i++) {
+    $total []= 0;
+}
+
+for($col = 0; $col < $N; $col++) {
+    $rowdata = array_map('intval', explode(' ', trim(fgets(STDIN))));
+    for($row = 0; $row < $M; $row++) {
+       $total[$row] += $rowdata[$row];
+    }
+}
+
+for($i = 0; $i < $M; $i++) {
+    if($total[$i] < $goal[$i]) {
+        echo "No";
+        exit;
+    }
+}
+echo "Yes";
